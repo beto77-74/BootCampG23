bec861eb19b61589ab07bb04990ea6d6049d3fca
Carlos Estupinan <carlos.estupinan@hotmail.com>
2025-01-29 21:00:32 -0500

    wwwwww

--- semana08/dia01-02-javascript-handman-game/main.js
+++ semana08/dia01-02-javascript-handman-game/main.js
@@ -10,11 +10,3 @@ const ALPHABET = Array.from({ length: 26 }, (_, index) => String.fromCharCode(65
 
 let correctGuess = ''
 let tries = 0
-
-
-
-/*for (let i = 65; i <= 90; i++) {
-    const button = document.createElement('button');
-    button.textContent = String.fromCharCode(i);
-    container.appendChild(button);
-}*/
