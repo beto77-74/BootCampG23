b2b7baf42126c66df3eec9e8d939f78b0e579d34
Carlos Estupinan <carlos.estupinan@hotmail.com>
2025-02-16 16:22:47 -0500

    pro 4

---
+++ Proyecto/proyecto-v-004/src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="d-flex justify-center w-96 uppercase p-3">
+                    <img className="fas ">
+                    </img> &nbsp;&nbsp; Pelicula MEG
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
